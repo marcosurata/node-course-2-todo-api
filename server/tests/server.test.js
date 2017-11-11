fc93953a7010cc03ed47cd2285b978a35dd0d466
marcosurata <marcosurata@gmail.com>
2017-11-11 17:25:54 -0200

    add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -12,7 +12,9 @@ const todos = [ {
 },
 {
     _id: new ObjectID(),
-    text:'second test todo'
+    text:'second test todo',
+    completed: true,
+    completedAt: 333
 }];
 
 
@@ -145,6 +147,43 @@ describe('DELETE /todos/:id', () => {
       .end(done);
   } );
 });
+
+describe('PATCH /todos/:id', () => {
+
+  it('should update a todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var newText = 'UPDATED test todo';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({text:newText, completed:true})
+      .expect(200)
+      .expect( (res) => {
+          expect(res.body.todo.text).toBe(newText);
+          expect(res.body.todo.completed).toBe(true);
+          expect(res.body.todo.completedAt).toBeA('number');
+
+      })
+      .end(done);
+  });
+
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({completed:false})
+      .expect(200)
+      .expect( (res) => {
+          expect(res.body.todo.completed).toBe(false);
+          expect(res.body.todo.completedAt).toBeFalsy();
+      })
+      .end(done);
+  });
+
+});
+
 
 
 });
